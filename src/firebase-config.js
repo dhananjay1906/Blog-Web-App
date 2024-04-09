67682f7e2ea406481b4dcee336531f1e9880016e
Dhananjay <56788799+dhananjay1906@users.noreply.github.com>
2024-04-09 12:26:03 -0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,12 +6,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDc7PJ9309GQRO0v2-mCUhBIWpOAnNyec8",
-  authDomain: "blogproject-92aa7.firebaseapp.com",
-  projectId: "blogproject-92aa7",
-  storageBucket: "blogproject-92aa7.appspot.com",
-  messagingSenderId: "135318509256",
-  appId: "1:135318509256:web:034fab8084a0ad39e3b7fd",
+  Update your firebase Config over here,
 };
 
 // Initialize Firebase
